b29df4cdf3b452cbdbcc75c33ae3bc8467079a07
Arthur Korchagin <lliepmah@mail.ru>
2017-09-22 00:12:20 +0300

    Initialized vectors

--- app/src/main/java/com/teachernavigator/teachernavigator/application/TeacherNavigatopApp.kt
+++ app/src/main/java/com/teachernavigator/teachernavigator/application/TeacherNavigatopApp.kt
@@ -1,6 +1,7 @@
 package com.teachernavigator.teachernavigator.application
 
 import android.app.Application
+import android.support.v7.app.AppCompatDelegate
 import com.example.root.androidtest.application.di.components.DaggerRootComponent
 import com.example.root.androidtest.application.di.components.RootComponent
 import com.example.root.androidtest.application.di.modules.RootModule
@@ -9,12 +10,13 @@ import com.orhanobut.hawk.Hawk
 /**
  * Created by root on 11.08.17.
  */
-class TeacherNavigatopApp :Application() {
+class TeacherNavigatopApp : Application() {
 
     private lateinit var mRootComponent: RootComponent
 
     override fun onCreate() {
         super.onCreate()
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
         Hawk.init(this).build()
         mRootComponent = DaggerRootComponent.builder()
                 .rootModule(RootModule(this))
